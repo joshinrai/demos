d6c30decb77fe925dd1770631d5a971f6bc5e351
joshinrai <joshinrai@163.com>
2017-05-15 09:06:37 +0800

    构建react项目
    构建react项目

---
+++ react/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>joshinrai的react项目</h2>
+        </div>
+        <p className="App-intro">
+          编辑<code>src/App.js</code>做修改
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/react.js
@@ -0,0 +1,2 @@
+var react = require('react') ;
+var reactdom = require('react-dom') ;
